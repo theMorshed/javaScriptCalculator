a6bbb0d8a8bd61e61341e611667e78b398404b1c
theMorshed <morshedfive@gmail.com>
2022-08-18 11:55:47 +0600

    write some fix class

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 function clearScreen() {
-  const result = document.getElementById('resultField');
-  result.value = '';
+    const result = document.getElementById('resultField');
+    result.value = '';
 }
 
 function calculate(value) {
-  const result = document.getElementById('resultField');
-  result.value += value;
+    const result = document.getElementById('resultField');
+    result.value += value;
 }
 
 function totalResult() {
-  let result = document.getElementById('resultField');
-  let oldValue = result.value;
-  let calculateNewValue = eval(oldValue);
-  result.value = calculateNewValue;
+    let result = document.getElementById('resultField');
+    let oldValue = result.value;
+    let calculateNewValue = eval(oldValue);
+    result.value = calculateNewValue;
 }
